1edd2c0e382688dc4cf348706cd6d9e3f7da9d83
LaughingLeader <laughingleader@gmail.com>
2021-08-09 17:35:22 -0500

    Junctioned project folders, added .gitignore, added attributes

---
+++ Mods/MaterialTweaks_0c56bb29-a167-4136-a89b-44e0525a0139/Story/RawFiles/Lua/BootstrapServer.lua
@@ -0,0 +1,11 @@
+Ext.Require("BootstrapShared.lua")
+
+-- Ext.RegisterListener("ModuleLoading", function()
+
+-- end)
+-- Ext.RegisterListener("SessionLoading", function()
+
+-- end)
+-- Ext.RegisterListener("SessionLoaded", function()
+
+-- end)
---
+++ Mods/MaterialTweaks_0c56bb29-a167-4136-a89b-44e0525a0139/Story/RawFiles/Lua/BootstrapShared.lua
@@ -0,0 +1,1 @@
+Ext.Require("Shared/FileOverrides.lua")
